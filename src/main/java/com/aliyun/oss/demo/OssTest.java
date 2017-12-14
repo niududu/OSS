8fd85ab66d8df0f7667f43a9130236b8d196a6ba
pc <pc@192.168.0.101>
2017-12-14 17:28:31 +0800

    123

---
+++ src/main/java/com/aliyun/oss/demo/OssTest.java
@@ -0,0 +1,61 @@
+package com.aliyun.oss.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.List;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.BucketInfo;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectListing;
+
+/*
+	 * OSS四大属性：
+	 * endpoint ：OSS域名
+	 * accessKsyID：访问ID
+	 * accessKeySecret:访问密钥
+	 * BucketName:Bucket是用来管理Object的存储空间
+	 */
+public class OssTest {
+	private static String endpoint="https://oss-cn-shenzhen.aliyuncs.com";
+	private static String accessKeyId="LTAIfTVYN8w5mmi1";
+	private static String accessKeySecret="fDLgyulAXElgA8IZXPuosidGbnvP4f";
+	private static String bucketName="niududu-new";
+	
+	public static void main(String[] args) {
+		//生成OSSClient对象
+		OSSClient ossClient=new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		//健壮性判断是否存在该Bucket对象
+		if(ossClient.doesBucketExist(bucketName)) {
+			System.out.println("该存储空间"+bucketName+"已存在");
+		}else {
+			System.out.println("该存储空间"+bucketName+"不存在，现在创建。");
+			ossClient.createBucket(bucketName);
+		}
+		BucketInfo bucketInfo = ossClient.getBucketInfo(bucketName);
+		System.out.println("Bucket："+bucketName+"信息如下");
+		System.out.println(bucketInfo.getBucket().getCreationDate());
+		System.out.println(bucketInfo.getBucket().getName());
+		System.out.println(bucketInfo.getBucket().getLocation());
+		System.out.println(bucketInfo.getBucket().toString());
+		
+		//上传图片文件的命名：
+		String  fileKey ="mobliePay1.jpg";
+//		final String keySuffixWithSlash="parent_directory/";
+//		ossClient.putObject(bucketName, keySuffixWithSlash, new ByteArrayInputStream(new byte[0]));
+		//从本地文件上传new File("本地文件路径")传
+		ossClient.putObject(bucketName,fileKey, new File("C://Users//pc//Desktop//官网图片/132.jpg"));
+		 System.out.println("Object：" + fileKey + "存入OSS成功。");
+		 
+		 ObjectListing objectListing = ossClient.listObjects(bucketName);
+         List<OSSObjectSummary> objectSummary = objectListing.getObjectSummaries();
+         System.out.println("您有以下Object：");
+         for (OSSObjectSummary object : objectSummary) {
+             System.out.println("\t" + object.getKey());
+         }
+         ossClient.deleteBucket("<bucketName>");
+         ossClient.shutdown();
+	}
+	
+
+}
---
+++ src/main/java/com/aliyun/oss/demo/Testdelete.java
@@ -0,0 +1,71 @@
+package com.aliyun.oss.demo;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.Bucket;
+import com.aliyun.oss.model.BucketInfo;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectListing;
+
+public class Testdelete {
+	private static String endpoint="oss-cn-beijing.aliyuncs.com";
+	private static String accessKeyId="LTAIfTVYN8w5mmi1";
+	private static String accessKeySecret="fDLgyulAXElgA8IZXPuosidGbnvP4f";
+	private static String bucketName="qwe-nie";
+	public static void main(String[] args) {
+		OSSClient ossClient=new OSSClient(endpoint, accessKeyId, accessKeySecret);
+		if(ossClient.doesBucketExist(bucketName)) {
+			System.out.println("已存在该存储区域，BucketName："+bucketName);
+		}else {
+			System.out.println("没有该"+bucketName+"存储区域，创建该存储区域。");
+			ossClient.createBucket(bucketName);
+		}
+		listObject(ossClient);
+		System.out.println("------图片上传--------");
+		//图片上传
+		upload(ossClient);
+		listObject(ossClient);
+		//将上传的文件删除
+		System.out.println("-------文件删除-------");
+		delete(ossClient);
+		System.out.println("--------------");
+		
+		listObject(ossClient);
+		
+		ossClient.shutdown();
+		
+		
+	}
+	private static void delete(OSSClient ossClient) {
+		if(ossClient.doesObjectExist(bucketName, "*.jpg")) {
+			System.out.println("该存储空间无内容 ，无法进行删除操作");
+		}else {
+			ossClient.deleteObject(bucketName, "four.jpg");
+			BucketInfo info=ossClient.getBucketInfo(bucketName);
+			System.out.println("删除成功");
+		}
+	}
+	/**
+	 * 上传图片文件
+	 * @param ossClient
+	 */
+	private static void upload(OSSClient ossClient) {
+		final  String fileKey1="three.jpg";
+		ossClient.putObject( bucketName,fileKey1, new File("C:\\Users\\pc\\Desktop\\官网图片\\netpay.jpg"));
+		final  String fileKey2="four.jpg";
+		ossClient.putObject( bucketName,fileKey2, new File("C:\\Users\\pc\\Desktop\\官网图片\\mobilepay.jpg"));
+			
+		}
+	private static void listObject(OSSClient ossClient) {
+		ObjectListing list = ossClient.listObjects(bucketName);
+		List<OSSObjectSummary> sum = list.getObjectSummaries();
+		for (OSSObjectSummary object : sum) {
+			System.out.println(object.getKey());
+		}
+	}
+	
+}
